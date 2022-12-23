26ab91a45431fdcfeaec78af93866befa2f0cbed
nikhiltavate <nikhiltavate4@gmail.com>
2022-12-23 19:11:12 +0530

    Added:code for welcome msg

---
+++ src/com/bridgelab_EmpWageUC/EmpWageUC.java
@@ -0,0 +1,9 @@
+package com.bridgelab_EmpWageUC;
+
+public class EmpWageUC {
+    public static void main(String[] args){
+        System.out.println("Wel come to Employee Wage Program");
+        }
+    }
+
+
